4504abdcf434db5bc663b227ae75ebe4c9be44df
GlowingPiranha <ikimus01@gmail.com>
2025-09-27 19:43:19 +0200

    [add] backend-trips

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import participantsRouter from "./routes/participantsRouter.js";
+import tripRouter from "./routes/tripRouter.js";
 
 const app = express();
 const PORT = 3001; // diverso da React (3000)
@@ -9,6 +10,7 @@ app.use(cors()); // permette al frontend di fare richieste
 app.use(express.json());
 
 app.use("/api/participants", participantsRouter);
+app.use("/api/trips", tripRouter);
 
 app.listen(PORT, () => {
   console.log(`Server attivo su http://localhost:${PORT}`);
